35c5ed255672644a90ac5985e101231f5ca4343a
Muammar Johnson <muammarj@gmail.com>
2021-05-01 01:46:44 -0400

    vmmv

--- app/src/main/java/com/example/t_mobile/view/MainActivity.kt
+++ app/src/main/java/com/example/t_mobile/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.t_mobile
+package com.example.t_mobile.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.t_mobile.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
