d97082656b849f4ffa771749cfff35ef51f9c597
Muammar Johnson <muammarj@gmail.com>
2021-05-02 00:32:16 -0400

    added button connected viewmodel to button

---
+++ app/src/main/java/com/example/t_mobile/viewModel/HomeViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.t_mobile.viewModel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+
+class HomeViewModel: ViewModel() {
+
+    private  val TAG = "HomeViewModel"
+
+    fun getTmobileResponse() {
+        Log.d(TAG, "tmobileresponse: get got")
+
+        }
+}
