54c624673840c96b4ae55f22582a8be7f8353954
Muammar Johnson <muammarj@gmail.com>
2021-05-03 18:57:31 -0400

    view shows all items

--- app/src/main/java/com/example/t_mobile/adapters/CardsAdapter.kt
+++ app/src/main/java/com/example/t_mobile/adapters/CardsAdapter.kt
@@ -1,16 +1,22 @@
 package com.example.t_mobile.adapters
 
+import android.app.ActionBar
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.t_mobile.databinding.CardItemBinding
 import com.example.t_mobile.model.Card
+import com.example.t_mobile.model.Image
 
 class CardsAdapter(
     private val results: List<Card>
 ) : RecyclerView.Adapter<CardsAdapter.TrackViewHolder>() {
+    private val TAG = "CardsAdapter"
 
     override fun onCreateViewHolder(
         parent: ViewGroup, viewType: Int
@@ -24,6 +30,10 @@ class CardsAdapter(
         Glide.with(image.context)
             .load(results[position].card?.image?.url.toString())
             .into(image)
+        if (results[position].card?.image == null){
+            Log.d(TAG, "onBindViewHolder: this image is null")
+            image.layoutParams.height = 300
+        }
         card = results[position]
 
 
